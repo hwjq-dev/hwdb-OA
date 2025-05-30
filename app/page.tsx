5256b0ae1dbf2ca290cfa44a27922073d7706675
hwjq-dev <hwjq0023@gmail.com>
2025-05-30 11:57:10 +0700

    chore: configure container add aspect-ratio plugin &scrollbar-plugin

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 const Page: NextPage = async () => {
   return (
-    <div className="p-8 space-y-2">
+    <div className="space-y-2 bg-red">
       <h2 className="text-4xl font-bold">欢迎来到好旺担保</h2>
       <p className=" leading-tight">
         我们为虚拟币、虚拟资产、机动车辆、房地产交易及产权过户等多种场景提供专业担保服务。
